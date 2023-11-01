5ea1899ba53e0106015b3539923680d4c5b83b23
ladyjaddy <129961187+ladyjaddy@users.noreply.github.com>
2023-11-01 10:41:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSomPom(idElentoAudio){
-    document.querySelector(idElentoAudio)
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio).play('');
+   
+    if(elemento !=null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        alert('Elenento não encontrado');
+    }
 }
 
 document.querySelectorAll('.tecla')
@@ -9,18 +15,22 @@ document.querySelectorAll('.tecla')
 for(contador = 0; < listadeTeclas.length; contador++){
 
 
-
-
    const instrumento = listadeTlecas[contador]
    
    const tecla = listadeTlecas[contador];
    
    const ideAudio = `#som_${instrumento}`;
 
-    tecla.onclick = fuction()
-        tocaSom(ideAudio);
+    tecla.onclick = fuction(){
+           tocaSom(ideAudio);  
     };
-        
-    
-    
-    
+        tecla.onkeydown = fuction(){
+            if(evento.code === 'Space'||eveneto.code === 'Enter' ){
+                tecla.classList.add('ativa');
+            }
+        }
+        tecla.onkeyup = fuction(){
+            tecla.classList.remove('ativa');
+        }
+            
+}
